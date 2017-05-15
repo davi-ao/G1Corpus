da3247cb003a3decd2fbcff2b6b688d78373679c
davi <davi.alvesoliveira@gmail.com>
2017-05-14 22:46:56 -0300

    v 0.2:  - Added comments spider

--- G1Corpus/spiders/comments.py
+++ G1Corpus/spiders/comments.py
@@ -5,16 +5,20 @@ import re
 
 class CommentsSpider(scrapy.Spider):
     name = 'comments'
-    start_urls = [
-        'http://g1.globo.com/index/feed/pagina-1.html'
-    ]
+    start_urls = []
+
+    def __init__(self):
+        super(CommentsSpider, self).__init__()
+
+        for i in range(451, 501):
+            self.start_urls.append('http://g1.globo.com/index/feed/pagina-' + str(i) + '.html')
 
     def parse(self, response):
         for href in [s.encode('ascii') for s in response.css('a.feed-post-link::attr(href)').extract()]:
             yield scrapy.Request(response.urljoin(href), callback=self.parse_news)
 
     def parse_news(self, response):
-        link = "http://comentarios.globo.com/comentarios/" + re.search("COMENTARIOS_URI:\s\"[a-z1-9\/-]*\"", response.css('script[id=SETTINGS]').extract()[0]).group(0).encode('ascii')[18:-1].replace('/', '@@') + "/" + re.search("COMENTARIOS_IDEXTERNO:\s\"[a-z0-9\/-]*\"", response.css('script[id=SETTINGS]').extract()[0]).group(0).encode('ascii')[24:-1].replace('/', '@@') + "/" + response.url.replace('/', '@@') + "/shorturl/" + re.search("TITLE:\s\"[^\n]*\"", response.css('script[id=SETTINGS]').extract()[0]).group(0).encode('latin-1')[8:-1] + "/1.json"
+        link = "http://comentarios.globo.com/comentarios/" + re.search("COMENTARIOS_URI:\s\"[a-z1-9\/-]*\"", response.css('script[id=SETTINGS]').extract_first()).group(0).encode('ascii')[18:-1].replace('/', '@@') + "/" + re.search("COMENTARIOS_IDEXTERNO:\s\"[a-z0-9\/-]*\"", response.css('script[id=SETTINGS]').extract_first()).group(0).encode('ascii')[24:-1].replace('/', '@@') + "/" + response.url.replace('/', '@@') + "/shorturl/" + re.search("TITLE:\s\"[^\n]*\"", response.css('script[id=SETTINGS]').extract_first()).group(0).encode('latin-1')[8:-1] + "/1.json"
         yield scrapy.Request(response.urljoin(link), callback=self.parse_comment)
 
     def parse_comment(self, response):
